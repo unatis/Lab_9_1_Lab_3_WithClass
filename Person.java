7fc32ae2f33a8189e13429220ee830d97f199b2c
Yuri Goncharov <ygpanzer@gmail.com>
2021-05-29 13:34:33 +0200

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Person {
+
+    String WorkerName;
+    String WorkerID;
+    int WorkerAge;
+    int WorkingExp;
+    int Salary = 0;
+    int Bonus = 0;
+    String WorkerPro;
+    int PaymentSum = 0;
+
+}
